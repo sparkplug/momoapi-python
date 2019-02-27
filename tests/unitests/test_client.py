80017b96efe209b0e15e5ba30099b819df4c4ea1
Moses Mugisha <mossplix@gmail.com>
2019-02-27 15:55:49 +0300

    add more mocks

--- tests/unitests/test_client.py
+++ tests/unitests/test_client.py
@@ -5,14 +5,17 @@ try:
     from unittest import mock
 except ImportError:
     import mock
+from requests import Request, Session
 
-from .utils import mocked_requests_get, mocked_requests_post
+from .utils import mocked_requests_get, mocked_requests_post, mocked_requests_session
 
 
 class TestClient(unittest.TestCase):
-    def setUp(self):
-        pass
-        #self.widget = Widget('The widget')
+
+    @mock.patch('requests.post', side_effect=mocked_requests_post)
+    def setUp(self, mock_get):
+        client = MomoApi("APIKEY", "USERID", "APISECRET")
+        self.client = client
 
     def tearDown(self):
         pass
@@ -26,8 +29,8 @@ class TestClient(unittest.TestCase):
         #request_mock.assert_requested("post", "/v1/accounts")
         assert isinstance(client, MomoApi)
 
-    @mock.patch('requests.post', side_effect=mocked_requests_post)
+    @mock.patch.object(MomoApi, "request", side_effect=mocked_requests_session)
     def test_request_to_pay(self, mock_get):
-        client = MomoApi("APIKEY", "USERID", "APISECRET")
-        ref = client.requestToPay("256772123456", "600", "123456789", note="dd",
-                                  message="dd", currency="EUR", environment="sandbox")
+
+        ref = self.client.requestToPay("256772123456", "600", "123456789", note="dd",
+                                       message="dd", currency="EUR", environment="sandbox")
--- tests/unitests/utils.py
+++ tests/unitests/utils.py
@@ -6,14 +6,18 @@ except ImportError:
     import mock
 
 
-def mocked_requests_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
+class MockResponse:
+    def __init__(self, json_data, status_code, headers={}):
+        self.json_data = json_data
+        self.status_code = status_code
+        self.headers = headers
 
-        def json(self):
-            return self.json_data
+    def json(self):
+        return self.json_data
+
+
+def mocked_requests_get(*args, **kwargs):
+
     import pdb
     pdb.set_trace()
 
@@ -26,19 +30,17 @@ def mocked_requests_get(*args, **kwargs):
 
 
 def mocked_requests_post(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-            return self.json_data
-    import pdb
-    pdb.set_trace()
 
     if '/collection/token/' in args[0]:
-        return MockResponse({"key1": "value1"}, 200)
-    elif args[0] == 'http://someotherurl.com/anothertest.json':
+        return MockResponse({"access_token": "token"}, 200)
+    elif "/collection/v1_0/requesttopay" in args[0]:
         return MockResponse({"key2": "value2"}, 200)
 
     return MockResponse(None, 404)
+
+
+def mocked_requests_session(*args, **kwargs):
+    if '/collection/token/' in args[1]:
+        return MockResponse({"access_token": "token"}, 200)
+    elif "/collection/v1_0/requesttopay" in args[1]:
+        return MockResponse({"key2": "value2"}, 200)
